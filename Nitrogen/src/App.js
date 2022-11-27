a730619b9120de7f5d6a27d00c7d67b463c2fabe
Laurentiul20 <81317143+Laurentiul20@users.noreply.github.com>
2022-11-27 18:39:54 -0500

    Add files via upload

---
+++ Nitrogen/src/App.js
@@ -0,0 +1,12 @@
+import map from './images/map.png';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <img src={map} alt="main map" height={700} width={700} />
+        </div>
+  );
+}
+
+export default App;
